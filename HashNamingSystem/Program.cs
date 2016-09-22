5e9e5f18efb10370e707c9abbb88ab55f0983dfd
markm <markm@DESKTOP-TJKED32>
2016-09-22 16:33:34 +0700

    Add project files.

---
+++ HashNamingSystem/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace HashNamingSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hash Naming System");
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("Enter Name Of Website:");
+            string siteName = Console.ReadLine().TrimStart(' ').TrimEnd(' ');
+
+            IPAddress ipAddress = ComputeHash(siteName);
+
+            Console.WriteLine(ipAddress.ToString());
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        static IPAddress ComputeHash(string siteName)
+        {
+            byte[] encodedPassword = new UTF8Encoding().GetBytes(siteName);
+            byte[] hash = ((HashAlgorithm)CryptoConfig.CreateFromName("MD5")).ComputeHash(encodedPassword);
+            return new IPAddress(hash);
+        }
+    }
+}
