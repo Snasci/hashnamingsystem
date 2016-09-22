b9eb3d4002d0f1b6d4ac301123378b0577d3c06c
Mark McCarron <mark.mccarron@live.co.uk>
2016-09-22 19:11:06 +0700

    Added IPv4 support and forced text to lower case

--- HashNamingSystem/Program.cs
+++ HashNamingSystem/Program.cs
@@ -12,11 +12,12 @@ namespace HashNamingSystem
             Console.WriteLine("Hash Naming System");
             Console.WriteLine(Environment.NewLine);
             Console.WriteLine("Enter Name Of Website:");
-            string siteName = Console.ReadLine().TrimStart(' ').TrimEnd(' ');
+            string siteName = Console.ReadLine().TrimStart(' ').TrimEnd(' ').ToLower();
 
             IPAddress ipAddress = ComputeHash(siteName);
 
             Console.WriteLine(ipAddress.ToString());
+            Console.WriteLine(ipAddress.MapToIPv4().ToString());
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
